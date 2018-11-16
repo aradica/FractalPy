a8d5b3f8174aa67e1be199775a350b23ec76d908
Andrija Radica <andrija.radica@gmail.com>
2018-11-16 19:52:27 +0100

    Cleaned up code

--- fractals.py
+++ fractals.py
@@ -1,19 +1,31 @@
 import turtle as tt
 
-def setPen(x, y, rotation):
+def setPen(pen, x, y, heading=None):
+    """
+    Place the Turtle() at a certain x and y.
+    If heading==None then it remains what it was before
+    """
     pen.penup()
     pen.setx(x)
     pen.sety(y)
     pen.pendown()
-    pen.setheading(rotation)
+    if heading != None:
+        pen.setheading(heading)
 
-def drawTriangle(edge):
+def drawTriangle(pen, edge):
+    """
+    Draws a triangle, takes edge size for input
+    Use with setPen() to choose location and rotation
+    """
     for k in range(3):
         pen.forward(edge)
         pen.left(120)
 
 
 class Ngon:
+    """
+    A general class for Ngons
+    """
     def __init__(self, edge, n, x=0, y=0, rotation=0):
         self.x = x
         self.y = y
@@ -27,20 +39,25 @@ class Ngon:
         return "Ngon object with {} vertices of size {}".format(self.n, self.edge)
 
     def draw(self, pen):
-        pen.penup()
-        pen.setx(self.x)
-        pen.sety(self.y)
-        pen.pendown()
-        pen.setheading(self.rotation)
+        """
+        Draw the Ngon with a Turtle() object
+        """
 
+        setPen(self.x, self.y, self.rotation)
         pen.begin_poly()
         for i in range(self.n):
             pen.forward(self.edge)
             pen.left(self.phi)
+        pen.end_poly()
 
         self.vertices = pen.get_poly()
 
 class SierpinskiTriangle:
+    """
+    edge: the largest vertice
+    iterations: 'depth'
+    x, y, rotation - optional coordinate system parameters
+    """
     def __init__(self, edge, iterations, x=0, y=0, rotation=0):
         self.x = x
         self.y = y
@@ -48,31 +65,39 @@ class SierpinskiTriangle:
         self.edge = edge
         self.iterations = iterations
 
-        self.generations = None
+        self.generations = self._makeGenerations()
 
 
-    def makeGenerations(self):
-        self.generations = [Ngon(self.edge/(2**i), 3) for i in range(self.iterations)]
+    def _makeGenerations(self):
+        return [Ngon(self.edge/(2**i), 3) for i in range(self.iterations)]
 
 
     def drawGeneration(self, i, x, y, pen):
-        setPen(x, y, self.rotation)
+        """
+        Draws the i-th generation with a Turtle() object
+        """
+        setPen(pen, x, y, self.rotation)
 
         pen.begin_poly()
-        drawTriangle(self.generations[i].edge)
+        drawTriangle(pen, self.generations[i].edge)
+        pen.end_poly()
         vertices = pen.get_poly()
         x1, y1 = vertices[1]
-        setPen(x1, y1, self.rotation)      
-        drawTriangle(self.generations[i].edge)
+        setPen(pen, x1, y1, self.rotation)      
+        drawTriangle(pen, self.generations[i].edge)
 
         x2, y2 = vertices[2]
-        setPen(x2, y2, self.rotation)
-        drawTriangle(self.generations[i].edge)
+        setPen(pen, x2, y2, self.rotation)
+        drawTriangle(pen, self.generations[i].edge)
 
         return [(x, y), (x1, y1), (x2, y2)]
     
 
     def drawAll(self, pen, iteration=0, g=None):
+        """
+        Draw the entire Sierpinski triangle
+        iteration and g variables should be left at their default states!
+        """
         print("Working on iteration:", iteration)
         if iteration == 0:
             g = self.drawGeneration(iteration, self.x, self.y, pen)
@@ -85,16 +110,15 @@ class SierpinskiTriangle:
                     self.drawAll(pen, iteration+1, g2)
 
 
+if __name__ == "__main__":
+    window = tt.Screen()
+    pen = tt.Turtle()
+    pen.speed(100)
 
-
-
-window = tt.Screen()
-pen = tt.Turtle()
-pen.speed(100)
-x = SierpinskiTriangle(200, 5, -100, -100)
-x.makeGenerations()
-#x.drawGeneration(0, 0, 0, pen)
-x.drawAll(pen)
-print(x.generations)
-
-window.exitonclick()
+    A = 200
+    ITERS = 5
+    X, Y = -150, -150
+    x = SierpinskiTriangle(A, ITERS, X, Y)
+    x.drawAll(pen)
+    print(x.generations)
+    window.exitonclick()
